087d7fec8c2492ae5b858e6e465f7d1c58468bd9
Vladislav Studenichnik <vstudenichnik@insoftgroup.com>
2023-04-18 20:51:45 +0300

    build

--- dist/settings.js
+++ dist/settings.js
@@ -1,5 +1,5 @@
-export { m as default } from './settings-937b96f1.js';
-import './index-512fc30d.js';
+export { m as default } from './settings-8d3c95f4.js';
+import './index-ca9cb73c.js';
 import 'qs';
 import 'deepmerge';
 import 'fast-case';
